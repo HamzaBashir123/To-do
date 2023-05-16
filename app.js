a741a61e8f52241c12108eab0f6737a938b6c623
hamzabashir1268@gmail.com <hamzabashir1268@gmail.com>
2023-05-17 01:15:56 +0500

    edit funtion set

--- app.js
+++ app.js
@@ -5,7 +5,12 @@ const message = document.querySelector(".message");
 const deleteAlContent = document.querySelector(".deleteAlContent");
 const EditButton = document.querySelector(".EditButton");
 
-let itemsArray = [];
+let editedUID = null;
+let itemsArray = JSON.parse(localStorage.getItem('myList')) || [];
+listDIv.innerHTML = itemsArray.join("");
+
+
+
 
 //  Enter press funtion
 textArea.addEventListener("keydown", (e) => {
@@ -14,6 +19,9 @@ textArea.addEventListener("keydown", (e) => {
   }
 });
 
+
+
+
 //  Submit button funtion
 const submitButton = () => {
   const uId = new Date().getTime();
@@ -21,88 +29,170 @@ const submitButton = () => {
   if (textArea.value === "") {
     alertInfo("input field is empty", "darkgoldenrod");
   } else {
-    const list = `<div class=" liDivSet" id="${uId}"><li class="newli" onclick="textClick('${uId}')"> ${textArea.value} </li><span onclick="Editfun('${uId}')" class="headingIcon listicon"><i class="fa-regular fa-pen-to-square"></i></span><span onclick="deleteFun('${uId}')"><img class="deleteImg headingIcon" src="./images/cross.png" alt=""></span> </div>`;
+    const list = `<div class="liDivSet" id="${uId}"><li class="newli" onclick="textClick('${uId}')"> ${textArea.value} </li><span onclick="Editfun('${uId}')" class="headingIcon listicon"><i class="fa-regular fa-pen-to-square"></i></span><span onclick="deleteFun('${uId}')"><img class="deleteImg headingIcon" src="./images/cross.png" alt=""></span> </div>`;
     itemsArray.push(list);
     listDIv.innerHTML = itemsArray.join("");
     alertInfo("{" + textArea.value + "} add your list", "green");
     textArea.value = "";
-    // console.log(itemsArray[1])
+    localStorage.setItem('myList', JSON.stringify(itemsArray))
   }
 };
 addButton.addEventListener("click", submitButton);
+
+
+
+
+
+
 
 //  text line through funtion
 const textClick = (uId1) => {
   for (i = 0; i < itemsArray.length; i++) {
     // console.log()
+    let innerText1 = listDIv.childNodes[i].firstChild.innerHTML;
+    console.log(typeof(innerText1))
     if (itemsArray[i].indexOf(uId1) != -1) {
-      // let divitem = listDIv.childNodes[i + i + 1];
-      // console.log(divitem);
-      listDIv.childNodes[i].firstChild.classList.toggle("checked");
-      // itemsArray.splice(i,1,listDIv.childNodes[i+i+1])
-      // listDIv.innerHTML = itemsArray.join("")
+      // listDIv.childNodes[ i || (i+1)].firstChild.classList.toggle("checked");
+      if(itemsArray[i].includes('checked')){
+        itemsArray.splice(
+          i,
+          1,
+          `<div class="liDivSet" id="${uId1}">
+            <li class="newli" onclick="textClick('${uId1}')">${innerText1}</li>
+            <span onclick="Editfun('${uId1}')" class="headingIcon listicon"><i class="fa-regular fa-pen-to-square"></i></span>
+            <span onclick="deleteFun('${uId1}')"><img class="deleteImg headingIcon" src="./images/cross.png" alt=""></span> </div>`
+        );
+        console.log(itemsArray[i])
+        listDIv.innerHTML = itemsArray.join("");
+        localStorage.setItem('myList', JSON.stringify(itemsArray))
+
+      }
+      else{
+        itemsArray.splice(
+          i,
+          1,
+          `<div class="liDivSet" id="${uId1}">
+            <li class="newli checked" onclick="textClick('${uId1}')"> ${innerText1} </li>
+            <span onclick="Editfun('${uId1}')" class="headingIcon listicon"><i class="fa-regular fa-pen-to-square"></i></span>
+            <span onclick="deleteFun('${uId1}')"><img class="deleteImg headingIcon" src="./images/cross.png" alt=""></span> </div>`
+        );
+        console.log(itemsArray[i])
+        listDIv.innerHTML = itemsArray.join("");
+        localStorage.setItem('myList', JSON.stringify(itemsArray))
+
+      }
+      
     }
   }
 };
+
+
+
+
+
 
 // Delete function
 
 const deleteFun = (uId1) => {
   for (i = 0; i < itemsArray.length; i++) {
     // console.log()
+
     if (itemsArray[i].indexOf(uId1) != -1) {
+      const myList = Array.from(listDIv.childNodes);
+      const filteredData = myList.filter((singleItem) => singleItem.id === uId1);
+       let textValue = filteredData[0].querySelector("li").innerText;
+      alertInfo("{" + textValue + "} delete your list", "red");
       itemsArray.splice(i, 1);
       listDIv.innerHTML = itemsArray.join("");
+      
+      localStorage.setItem('myList', JSON.stringify(itemsArray))
+
     }
   }
 };
 
+
+
+
+
+
+
 // Edti Fucntion List contetn copy to imput box
 
 const Editfun = (uId1) => {
-  
-  const newitemsArray = Array.from(listDIv.childNodes)
-  
+  // const newitemsArray = Array.from(listDIv.childNodes)
+  // var fillterDAta = newitemsArray.filter((item) => item.id === uId1 )
+  //        textArea.value   = fillterDAta[0].querySelector("li").innerHTML;
+  //        EditButton.classList.add("hidden")
+  //        EditButton.innerHTML = "Edit";
+  //        console.log(fillterDAta)
 
-  var fillterDAta = newitemsArray.filter((item) => item.id === uId1 )
-         textArea.value   = fillterDAta[0].querySelector("li").innerHTML;
-         EditButton.classList.add("hidden")
-         EditButton.innerHTML = "Edit";
-         console.log(fillterDAta)
+  //        EditButton.addEventListener("click",()=> editProcess(uId1))
+  //        console.log(uId1)
 
-         EditButton.addEventListener("click",()=> editProcess(uId1))
-         console.log(uId1)
+
+  alertInfo("Please edit your item ", "darkgoldenrod");
+
+  editedUID = uId1;
+  EditButton.classList.add("hidden")
+  EditButton.innerHTML = "Edit";
+  
+  
+  EditButton.addEventListener("click", editProcess);
+  const myList = Array.from(listDIv.childNodes);
+  const filteredData = myList.filter((singleItem) => singleItem.id === uId1);
+  textArea.value = filteredData[0].querySelector("li").innerText;
+};
+
+
+
+
+// Edit process
+
+const editProcess = () => {
+  
+  const indexNum = itemsArray.findIndex((item) => item.includes(editedUID));
+  
+  itemsArray.splice(
+    indexNum,
+    1,
+    `<div class="liDivSet" id="${editedUID}">
+      <li class="newli" onclick="textClick('${editedUID}')"> ${textArea.value} </li>
+      <span onclick="Editfun('${editedUID}')" class="headingIcon listicon"><i class="fa-regular fa-pen-to-square"></i></span>
+      <span onclick="deleteFun('${editedUID}')"><img class="deleteImg headingIcon" src="./images/cross.png" alt=""></span> </div>`
+  );
+  listDIv.innerHTML = itemsArray.join("");
+  localStorage.setItem('myList', JSON.stringify(itemsArray))
+
+
+  textArea.value = "";
+
+  alertInfo("{" + textArea.value + "} Edit your Item", "darkgoldenrod");
+
+
+  EditButton.classList.remove("hidden");
+  EditButton.innerHTML = "";
+  
+};
+
+
+
+// Delete All funtion
+
+const deleteall = () => {
+  if (listDIv.innerHTML == "") {
+    alertInfo("Sorry no item in the list", "darkgoldenrod");
+  } else {
+    if (confirm("Are you sure all items  are deleted")) {
+      itemsArray = [];
+      localStorage.setItem('myList', JSON.stringify(itemsArray))
+      listDIv.innerHTML = "";
+      textArea.value= "";
+      alertInfo("All items successfully deleted", "red");
     }
-
-
-  // Edit process
-
-
-
-   const editProcess = (uId12) => {
-    console.log(uId12)
-    const indexNum  = itemsArray.findIndex((item)=> item.includes(uId12))
-    console.log(indexNum)
-    itemsArray.splice(
-      indexNum,
-      1, 
-      `<div class=" liDivSet" id="${uId12}">
-      <li class="newli" onclick="textClick('${uId12}')"> ${textArea.value} </li>
-      <span onclick="Editfun('${uId12}')" class="headingIcon listicon"><i class="fa-regular fa-pen-to-square"></i></span>
-      <span onclick="deleteFun('${uId12}')"><img class="deleteImg headingIcon" src="./images/cross.png" alt=""></span> </div>`
-    );
-    listDIv.innerHTML = itemsArray.join("");
-    EditButton.classList.remove("hidden");
-    EditButton.innerHTML = "";
-    indexNum = "";
-    
-    
-  };
-
-
-
-
-
+  }
+};
+deleteAlContent.addEventListener("click", deleteall);
 
 
 
@@ -116,26 +206,4 @@ const alertInfo = (info, color) => {
   setTimeout(() => {
     message.style.visibility = "hidden";
   }, 2000);
-};
-
-// Delete All funtion
-
-const deleteall = () => {
-  if (listDIv.innerHTML == "") {
-    alertInfo("Sorry no item in the list", "darkgoldenrod");
-  } else {
-    if (confirm("Are you sure all items  are deleted")) {
-      itemsArray = [];
-      listDIv.innerHTML = "";
-      alertInfo("All items successfully deleted", "red");
-    }
-  }
-};
-
-deleteAlContent.addEventListener("click", deleteall);
-
-// Delete Single item
-
-const deleteitem = (uid) => {
-  console.log(uid);
 };
